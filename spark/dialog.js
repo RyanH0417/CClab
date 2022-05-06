61c9522ab756007dcfca809e1e5b973ccd18e698
RyanH0417 <2371161067@qq.com>
2022-05-06 14:11:50 +0800

    dialog intro

---
+++ spark/dialog.js
@@ -0,0 +1,44 @@
+let dialog1;
+let dialog2;
+let dialog3;
+let dialog4;
+let dialog5;
+
+function setup() {
+  noCanvas();
+  dialog1 = select('#dialog1');
+  dialog2 = select('#dialog2');
+  dialog3 = select('#dialog3');
+  dialog4 = select('#dialog4');
+  dialog5 = select('#dialog5');
+  dialog1.mousePressed(dialog1Clicked);
+  dialog2.mousePressed(dialog2Clicked);
+  dialog3.mousePressed(dialog3Clicked);
+  dialog4.mousePressed(dialog4Clicked);
+}
+
+function draw() {
+  background(220);
+
+  if (frameCount == .700) {
+    dialog1.style('opacity', '.7');
+  }
+}
+
+function dialog1Clicked() {
+  dialog2.style('opacity', '.7');
+  dialog1.style('opacity','0')
+}
+
+function dialog2Clicked() {
+  dialog3.style('opacity','.7');
+  dialog2.style('opacity','0')
+}
+function dialog3Clicked() {
+  dialog4.style('opacity', '.7');
+  dialog3.style('opacity','0')
+}
+function dialog4Clicked() {
+  dialog5.style('opacity', '.7');
+  dialog4.style('opacity','0')
+}
